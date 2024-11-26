247dff65cbc898f28fde9f7fff3b9331c94c8ba3
minhajshafiq <minhaj93500@gmail.com>
2024-11-26 16:03:38 +0100

    Added axios routes to some register files

--- app/hub/(register)/VerifyEmailInscription.tsx
+++ app/hub/(register)/VerifyEmailInscription.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'expo-router';
+import axios from 'axios';
 import VerifyPinCode from '@/components/verifyPinCode/VerifyPinCode';
+import { Alert } from 'react-native';
 
 const ForgotPasswordVerifyEmail: React.FC = () => {
   const router = useRouter();
 
-  const handleVerification = (pinCode: string) => {
-    // Implémenter la logique pour le vérifier le PIN code ici
-    router.push('/hub/(register)/ContactPreferences');
+  const handleVerification = async (pinCode: string) => {
+    if (!pinCode) {
+      Alert.alert('Error', 'Please enter the PIN code.');
+      return;
+    }
+
+    try {
+      // Remplacez l'URL par celle de votre API
+      const response = await axios.post('xxxxx', {
+        pinCode,
+      });
+
+      if (response.status === 200 && response.data.valid) {
+        // Le code PIN est valide, on redirige l'utilisateur
+        router.push('/hub/(register)/ContactPreferences');
+      } else {
+        Alert.alert('Invalid PIN', 'The PIN code you entered is incorrect.');
+      }
+    } catch (error) {
+      console.error('Error verifying PIN:', error);
+      Alert.alert('Error', 'An error occurred while verifying your PIN.');
+    } finally {
+    }
   };
 
   return (
