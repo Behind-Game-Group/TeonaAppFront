3c1ecde7f2943732f4ef5dbefa914d928027d3a6
Jessy <Jessy@mytt.com>
2025-01-15 11:15:41 +0100

    lastRate

--- app/settings/Rate.tsx
+++ app/settings/Rate.tsx
@@ -1,13 +1,18 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native';
 import React, { useState } from 'react';
 import { IoStarSharp } from 'react-icons/io5';
-import { Picker } from '@react-native-picker/picker';
+import { GoChevronDown } from 'react-icons/go';
+import Modal from './modal';
 
 //!isVisible == true ? styles.on : styles.off
 export default function RateTheApp() {
   const [selecrStars, setSelectStars] = useState<number>(0);
-  const [selectOption1, setSelectOption1] = useState<string>('');
-  const [selectOption2, setSelectOption2] = useState<string>('');
+  const [selectOption1, setSelectOption1] = useState<string>(
+    'please choose an option',
+  );
+  const [selectOption2, setSelectOption2] = useState<string>(
+    'please choose an option',
+  );
   const [option1] = useState<string[]>([
     'A compliment',
     'Something to improve',
@@ -21,7 +26,17 @@ export default function RateTheApp() {
     'Travel follow-up (eg.Flight status)',
     'My Teona Group account',
   ]);
+  const [modal, setModal] = useState<boolean>(false);
+  const [hover, setHover] = useState<number>(0);
+  const [modal2, setModal2] = useState<boolean>(false);
+  const [hover2, setHover2] = useState<number>(0);
   const stars = [1, 2, 3, 4, 5];
+  const changeModal = () => {
+    setModal(!modal);
+  };
+  const changeModal2 = () => {
+    setModal2(!modal2);
+  };
   // setCountries(months);
   return (
     <View>
@@ -39,38 +54,83 @@ export default function RateTheApp() {
           </Pressable>
         ))}
       </Text>
-      <Picker
-        selectedValue={selectOption1}
-        onValueChange={(itemValue) => setSelectOption1(itemValue)}
-        style={styles.picker}
-      >
-        <Picker.Item
-          label='please choose an option'
-          value='please choose an option'
-        />
-        {option1.map((option) => (
-          <Picker.Item key={option} label={option} value={option} />
+      {}
+      <Pressable style={styles.picker} onPress={changeModal}>
+        <Text style={{ width: '95%' }}> {selectOption1}</Text>
+        <GoChevronDown style={{ textAlign: 'right' }} />
+      </Pressable>
+      <View style={modal ? styles.modal : styles.off}>
+        {' '}
+        {option1.map((option, id) => (
+          <Pressable
+            style={id == hover ? styles.test : { paddingLeft: 5 }}
+            onHoverIn={() => {
+              setHover(id);
+            }}
+            onPress={() => {
+              setSelectOption1(option);
+              changeModal();
+            }}
+          >
+            {option}
+          </Pressable>
         ))}
-      </Picker>
-      <Picker
-        selectedValue={selectOption2}
-        onValueChange={(itemValue) => setSelectOption2(itemValue)}
-        style={styles.picker}
-      >
-        <Picker.Item
-          label='please choose an option'
-          value='please choose an option'
-        />
-        {option2.map((option) => (
-          <Picker.Item key={option} label={option} value={option} />
+      </View>
+      <Pressable style={styles.picker} onPress={changeModal2}>
+        <Text style={{ width: '95%' }}> {selectOption2}</Text>
+        <GoChevronDown style={{ textAlign: 'right' }} />
+      </Pressable>
+
+      <View style={modal2 ? styles.modal : styles.off}>
+        {' '}
+        {option2.map((option, id) => (
+          <Pressable
+            style={id == hover2 ? styles.test : { paddingLeft: 5 }}
+            onHoverIn={() => {
+              setHover2(id);
+            }}
+            onPress={() => {
+              setSelectOption2(option);
+              changeModal2();
+            }}
+          >
+            {option}
+          </Pressable>
         ))}
-      </Picker>
-      {}{' '}
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  modal: {
+    display: 'flex',
+    top: 20,
+    backgroundColor: '#FFF',
+    borderColor: '#888',
+    borderRadius: 15,
+    borderWidth: 2,
+    padding: 5,
+    paddingLeft: 10,
+    alignSelf: 'center',
+    width: '95%',
+  },
+  off: { display: 'none' },
+  test: {
+    backgroundColor: '#AAA', //orange
+    borderRadius: 5,
+    paddingLeft: 5,
+  },
   starsOn: { margin: 4, width: 45, height: 45, color: 'yellow' },
   stars: { margin: 4, width: 45, height: 45 },
-  picker: { backgroundColor: '#FFF', marginTop: 35, width: '95%' },
+  picker: {
+    backgroundColor: '#FFF',
+    marginTop: 35,
+    paddingLeft: 10,
+    width: '95%',
+    alignSelf: 'center',
+    borderColor: '#888',
+    borderRadius: 15,
+    borderWidth: 2,
+    flexDirection: 'row',
+  },
 });
